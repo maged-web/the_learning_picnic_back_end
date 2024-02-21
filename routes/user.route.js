6d3bc350426f0abee6f0f14b221ac8c9b5212825
YarraMohamed <yara.med014@gmail.com>
2024-02-21 11:27:46 +0200

    -V3

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const router = express.Router();
 const userController = require("../controllers/user.controller")
+const allowedTo = require("../middleware/allowedTo")
+const userRoles = require("../utils/userRoles")
+const verifyToken = require("../middleware/verifyToken")
 
 
 router.route("/register")
-    .post(userController.register)
+    .post(verifyToken,allowedTo(userRoles.ADMIN),userController.register)
 
 router.route("/login")
     .post(userController.login)
+
+router.route("/:id")
+      .get(verifyToken,allowedTo(userRoles.ADMIN),userController.viewAccount)
+      .delete(verifyToken, allowedTo(userRoles.ADMIN),userController.deleteUser)
+      .put(verifyToken,allowedTo(userRoles.ADMIN),userController.updateUser)
+
+router.route("/")
+      .get(verifyToken,allowedTo(userRoles.ADMIN),userController.getAllUser)
+
+router.route("/parent/:id")
+      .put(verifyToken,allowedTo(userRoles.ADMIN),userController.addChildEmail)
+
 
 /* router.route("/logout")
     .post(userController.logout) */
